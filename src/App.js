d6cf0e8caf1f29b7f99423822fb1478ef16dec62
Aldo A. Avalos <99375833+aldorithms@users.noreply.github.com>
2023-02-28 19:44:50 -0800

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import Login from "./components/Login";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { useState } from "react"
+import AuthContext from "./context/AuthContext"
+import NavigationBar from './components/NavigationBar'
+import HomePage from './components/Homepage'
+
+const App = () => {
+  const [authstatus, setauthstatus] = useState(false);
+  const login = () => {
+    setauthstatus(true);
+  };
+  const logout = () => {
+    setauthstatus(false);
+  };
+  return (
+    <AuthContext.Provider value={{ status: authstatus, login: login, logout: logout }}>
+      {authstatus ? <>
+        <NavigationBar />
+        <HomePage />
+      </> : <Login />}
+    </AuthContext.Provider>
+  )
+}
+export default App;
---
+++ src/HomePage.jsx
@@ -0,0 +1,46 @@
+const HomePage = () => {
+    const [openModal, setOpenModal] = useState(false)
+    const { logout } = useContext(AuthContext);
+    const handleIdle = () => {
+        setOpenModal(true);
+    }
+    const {idleTimer} = useIdle({ onIdle: handleIdle, idleTime: 5 })
+    const stay = () => {
+        setOpenModal(false)
+        idleTimer.reset()
+    }
+    const handleLogout = () => {
+        logout()
+        setOpenModal(false)
+    }
+
+    return <Container className="mt-4">
+        <Row>
+            <Col></Col>
+            <Col>You are now logged in </Col>
+            <Col></Col>
+        </Row>
+        <Modal show={openModal} onHide={stay}>
+            <Modal.Header closeButton>
+                <Modal.Title>Your session is about to expire</Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+                <p>
+                    Your session is about to expire. You'll be automatically signed out.
+                </p>
+                <p>
+                    Do you want to stay signed in?
+                </p>
+            </Modal.Body>
+            <Modal.Footer>
+                <Button variant="secondary" onClick={handleLogout}>
+                    Sign out now
+                </Button>
+                <Button variant="primary" onClick={stay}>
+                    Stay signed in
+                </Button>
+            </Modal.Footer>
+        </Modal>
+    </Container>
+}
+export default HomePage;
